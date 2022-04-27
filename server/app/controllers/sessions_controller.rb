b671da885bc2a9693c2a89b178588c499e0c2063
adarshmanwal <adarsh.manwal@oncehub.com>
2022-04-27 12:21:37 +0530

    authentication token achived

--- server/app/controllers/sessions_controller.rb
+++ server/app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
+
   def create 
     user=User.where(email: params[:email]).first
-
     if user==nil 
       render json: { error: true, message: 'User not found !'}, 
       status: 401 and return
@@ -20,6 +20,17 @@ class SessionsController < ApplicationController
   end 
   
   def destroy
+    # puts("=========",current_user.authentication_token)
+    if current_user==nil 
+      render json: { error: true, message: 'no current User Login'}, status: 200 and return
+
+    end
+    current_user.invalidate_all_sessions!
+    # current_user.destroy
+    render json: { error: true, message: 'Please confirm your registered email to access your account.'},
+    status: 500 and return
+
+    # status: 202 and return
   end
 
 end
